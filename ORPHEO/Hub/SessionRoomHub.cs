fa5670696915c476a23a7d8a1b36d70efdec94f8
Dorneanu Diana <dianador2005@gmail.com>
2025-12-18 11:55:16 +0200

    signalR

---
+++ ORPHEO/Hub/SessionRoomHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Orpheo.Hubs
+{
+    public class SessionRoomHub : Hub
+    {
+        public async Task JoinRoom(int roomId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, $"room-{roomId}");
+        }
+
+        public async Task PlaySong(int roomId, int songIndex, double currentTime)
+        {
+            await Clients
+                .Group($"room-{roomId}")
+                .SendAsync("PlaySong", songIndex, currentTime);
+        }
+
+        public async Task PauseSong(int roomId, double currentTime)
+        {
+            await Clients
+                .Group($"room-{roomId}")
+                .SendAsync("PauseSong", currentTime);
+        }
+        public async Task SeekSong(int roomId, double currentTime)
+        {
+            await Clients
+                .Group($"room-{roomId}")
+                .SendAsync("SeekSong", currentTime);
+        }
+
+    }
+}
